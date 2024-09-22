7f01cf0f490b2329415311b2bb969361ffb0b5fc
WangRui <wangrui@betalpha.com>
2024-09-22 17:41:35 +0800

    feat(支持创建新的比赛):
    整个系统只能有一个状态为 1 的比赛，如果查到目前还有状态为 1 的，会将它置为 2

--- src/handlers/match_handlers.rs
+++ src/handlers/match_handlers.rs
@@ -13,29 +13,28 @@ pub async fn create_match(req: &mut Request, res: &mut Response) {
         .fetch_all(get_db_pool()).await;
 
     for item in &result.unwrap() {
-        let id: Result<i32, Error> = item.try_get(0);
-        update_status(&id.unwrap());
+        let id: Result<String, Error> = item.try_get(0);
+        update_status(&id.unwrap()).await;
     }
 
     let match_info_req: MatchInfoRequest = req.parse_json::<MatchInfoRequest>().await.unwrap();
     println!("创建一个新的比赛");
-    save_to_db(match_info_req).await;
+    save_to_db(&match_info_req).await;
     res.headers_mut()
         .insert("Content-Type", HeaderValue::from_static("application/json"));
 }
 
-async fn update_status(x: &i32) -> Result<(), Error> {
-    let query = "UPDATE rs_activity SET status = 2 WHERE id = $1";
-    let id = 1;
+async fn update_status(x: &str) -> Result<(), Error> {
+    let query = "UPDATE rs_activity SET status = 2 WHERE id = ?";
     let result = sqlx::query(query)
-        .bind(&id)
+        .bind(x)
         .execute(get_db_pool())
         .await?;
-    println!("将 id {} 为的比赛状态改为 {}", x, &id);
+    println!("将 id {} 为的比赛状态改为 {}", x, 2);
     Ok(())
 }
 
-async fn save_to_db(match_info_req: MatchInfoRequest) -> Result<MySqlQueryResult, Error> {
+async fn save_to_db(match_info_req: &MatchInfoRequest) -> Result<MySqlQueryResult, Error> {
     let uuid = Uuid::new_v4();
     let pool = get_db_pool();
     let result = sqlx::query!(
@@ -52,6 +51,28 @@ async fn save_to_db(match_info_req: MatchInfoRequest) -> Result<MySqlQueryResult
         .execute(pool)
         .await;
 
+    if match_info_req.opposing.is_some() {
+        let result = sqlx::query!(
+        "INSERT INTO rs_activity_info (activity_id, color, opposing, opposing_color) VALUES (?, ?, ?, ?)",
+        uuid.to_string(),
+        match match_info_req.color.as_ref(){
+                Some(ref x) => x,
+                None => "#FFFFFF",
+            },
+        match match_info_req.opposing.as_ref(){
+                Some(ref x) => x,
+                None => "待定",
+            },
+        match match_info_req.opposing_color.as_ref(){
+                Some(ref x) => x,
+                None => "#483D8B",
+            }
+    )
+            .execute(pool)
+            .await;
+    }
+
+
     result
 }
 
@@ -64,6 +85,9 @@ struct MatchInfoRequest {
     holding_date: NaiveDateTime,
     location: String,
     status: i8,
+    color: Option<String>,
+    opposing: Option<String>,
+    opposing_color: Option<String>,
 }
 
 #[cfg(test)]
@@ -76,15 +100,17 @@ mod tests {
         let make_db_pool = crate::make_db_pool().await;
         DB_POOL.set(make_db_pool).ok();
 
-        let result = save_to_db(MatchInfoRequest {
+        let result = save_to_db(&MatchInfoRequest {
             name: "".to_string(),
             cover: "".to_string(),
             start_time: Default::default(),
             end_time: Default::default(),
             holding_date: Default::default(),
             location: "".to_string(),
-
             status: 10i8,
+            color:Some(String::from("xxxx")),
+            opposing:Some(String::from("xxxx")),
+            opposing_color:Some(String::from("xxxx")),
         }).await;
         match result {
             Ok(_) => {
